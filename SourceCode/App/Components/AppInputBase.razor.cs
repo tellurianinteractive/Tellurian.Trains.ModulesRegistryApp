8c4a0e692a3a7e588a0586a6a889b360eaffb2e0
Stefan Fjällemark <fjallemark@hotmail.com>
2021-04-07 15:07:05 +0200

    Refactoring of AppInputBase.

--- SourceCode/App/Components/AppInputBase.razor.cs
+++ SourceCode/App/Components/AppInputBase.razor.cs
@@ -1,18 +1,19 @@
 ﻿using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Forms;
 using System;
 using System.Linq.Expressions;
 
 namespace ModulesRegistry.Components
 {
-    public abstract class AppInputBase<TValue> : InputBase<TValue>
+    public abstract partial class AppInputBase<TValue> 
     {
+
         [Parameter] public string? Label { get; set; }
         [Parameter] public Expression<Func<string>>? ValidationFor { get; set; }
         [Parameter] public int? Width { get; set; }
         [Parameter] public bool IsDisabled { get; set; }
+        [Parameter] public bool IsVisible { get; set; } = true;
+        [Parameter] public RenderFragment? ChildContent { get; set; }
 
-        protected string WidthCss => Width.HasValue ? $"col-md-{Width}" : "col-md-4";
-
+        public string WidthCss => Width.HasValue ? $"col-md-{Width}" : "col-md-4";
     }
 }
